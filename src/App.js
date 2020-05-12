a2ac37e5379c3f282644d6030ab72c411a9b7355
codecamp-official-thailand <codecamp.official.thailand@gmail.com>
2020-05-12 14:30:25 +0700

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Persons from './containers/Persons';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <ol>
         <li>เปลี่ยน App นี้ไปใช้ Redux ทั้งหมด (ไม่ใช้ State ใน Component)</li>
       </ol>
+      <Persons />
     </div>
   );
 }
